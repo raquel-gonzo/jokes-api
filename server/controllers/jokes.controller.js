c7d827164dc7438796c274c37729b9d3b65a2001
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-11 19:06:44 -0500

    controller file contains CRUD operations. modularized folder structure is correct.

--- server/controllers/jokes.controller.js
+++ server/controllers/jokes.controller.js
@@ -1,32 +1,33 @@
 const Joke = require("../models/jokes.model");
 
 // functions go here for the remaining of the assignment.
+// CRUD operations
 module.exports.getAllJokes = (req, res) => {
-    Joke.find()
-    .then()
-    .catch()
+    Joke.find()         // returns promise
+    .then((jokesArray) => {res.json(jokesArray)})             // name a package of data.
+    .catch((err) => {res.json(err)})
 };
 
 module.exports.getOneJoke = (req, res) => {
-    Joke.findOne()
-    .then()
-    .catch()
+    Joke.findOne((oneJoke))
+    .then((oneJoke) => {res.json(jokesArray)})
+    .catch((err) => {res.json(err)})
 };
 
 module.exports.createNewJoke = (req, res) => {
     Joke.create(req.body)
-    .then()
-    .catch()
+    .then((createJoke) => {res.json(createJoke)})
+    .catch((err) => {res.json(err)})
 };
 
 module.exports.updateExistingJoke = (req, res) => {
-    Joke.findByIdAndUpdate()
-    .then()
-    .catch()
-}
+    Joke.findByIdAndUpdate(req.params.id, req.body)
+    .then((updateJoke) => {res.json(updateJoke)})
+    .catch((err) => {res.json(err)})
+};
 
 module.exports.deleteExistingJoke = (req, res) => {
-    Joke.deleteOne()
-    .then()
-    .catch()
-}
+    Joke.findByIdAndDelete(req.params.id)
+    .then((deleteJoke) => {res.json(deleteJoke)})
+    .catch((err) => {res.json(err)})
+};
